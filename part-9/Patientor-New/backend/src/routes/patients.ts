f3e91c03bb04818a65c367590c2734bd0d8bd6f4
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-05-15 09:14:06 +0100

    9.26: added a todo list: almost blocked :)

--- part-9/Patientor-New/backend/src/routes/patients.ts
+++ part-9/Patientor-New/backend/src/routes/patients.ts
@@ -32,4 +32,26 @@ patientRouter.get("/:id", (_req, res) => {
   }
 });
 
+// Adding the steps I think I need to take. I am almost 100% blocked on this one so might need to go away, upskill and return to the course.
+
+// 1. Add a new route in patientRouter for the /api/patients/:id/entries endpoint.
+
+// 2. Create a new controller function for this route that accepts a request and response object as arguments.
+
+// 3. Extract the id parameter from the request URL using req.params.id.
+
+// 4. Use the findPatientById function to find the patient with the given id.
+
+// 5. Parse the request body to extract the new entry object.
+
+// 6. Use a function to validate that the new entry object has all required fields for its type.
+
+// 7. Add the new entry object to the patient's entries array.
+
+// 8. Save the updated patient to the patients array.
+
+// 9. Return the updated patient object in the response.
+
+// If there is an error, return a response with an appropriate error message and status code.
+
 export default patientRouter;
