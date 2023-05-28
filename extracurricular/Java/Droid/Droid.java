89aa0d10da8f86e6171899b8acc15d351a94172f
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2023-05-28 12:05:21 +0100

    Codecademy Droid

---
+++ extracurricular/Java/Droid/Droid.java
@@ -0,0 +1,29 @@
+package Droid;
+
+public class Droid {
+    String name;
+    int batteryLevel;
+
+    public Droid(String droidName) {
+        name = droidName;
+        batteryLevel = 100;
+    }
+
+    public String toString() {
+        return "Hello, I'm the droid: " + name;
+    }
+
+    public void performTask(String task) {
+        System.out.println(name + " is performing task: " + task);
+        batteryLevel -= 10;
+    }
+
+    public static void main(String[] args) {
+        Droid codey = new Droid("Codey");
+        System.out.println(codey);
+        codey.performTask("dancing");
+        codey.performTask("cooking");
+        codey.performTask("cleaning");
+
+    }
+}
