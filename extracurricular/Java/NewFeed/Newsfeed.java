04bdb75e71ee8162cf8359525b8417bac50ea0f2
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2023-05-30 10:28:32 +0100

    Codecademy Newsfeed

---
+++ extracurricular/Java/NewFeed/Newsfeed.java
@@ -0,0 +1,56 @@
+package NewFeed;
+
+public class Newsfeed {
+  
+  // Array to store trending article titles
+  String[] trendingArticles;
+  
+  // Array to store the number of views for each article
+  int[] views;
+  
+  // Array to store the ratings for each article
+  double[] ratings;
+  
+  // Constructor that initializes the arrays with initial values
+  public Newsfeed(String[] initialArticles, int[] initialViews, double[] initialRatings){
+    trendingArticles = initialArticles;
+    views = initialViews;
+    ratings = initialRatings;
+  }
+  
+  // Method to get the top article from the trending articles array
+  public String getTopArticle(){
+    return trendingArticles[0];
+  }
+  
+  // Method to increment the view count for a specific article
+  public void viewArticle(int articleNumber){
+    views[articleNumber] = views[articleNumber] + 1;
+    System.out.println("The article '" + trendingArticles[articleNumber] + "' has now been viewed " + views[articleNumber] + " times!");
+  }
+  
+  // Method to change the rating for a specific article
+  public void changeRating(int articleNumber, double newRating){
+    if (newRating > 5 || newRating < 0) {
+      System.out.println("The rating must be between 0 and 5 stars!");
+    } else {
+      ratings[articleNumber] = newRating;
+      System.out.println("The article '" + trendingArticles[articleNumber] + "' is now rated " + ratings[articleNumber] + " stars!");
+    }
+  }
+  
+  // Entry point of the program
+  public static void main(String[] args){
+    // Create a new instance of the Newsfeed class with initial values
+    String[] robotArticles = {"Oil News", "Innovative Motors", "Humans: Exterminate Or Not?", "Organic Eye Implants", "Path Finding in an Unknown World"};
+    int[] robotViewers = {87, 32, 13, 11, 7};
+    double[] robotRatings = {2.5, 3.2, 5.0, 1.7, 4.3};
+    Newsfeed robotTimes = new Newsfeed(robotArticles, robotViewers, robotRatings);
+    
+    // Call methods on the robotTimes instance to simulate interactions
+    robotTimes.viewArticle(2);
+    robotTimes.viewArticle(2);
+    System.out.println("The top article is " + robotTimes.getTopArticle());
+    robotTimes.changeRating(3, 5);
+  }
+}
